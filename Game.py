be80991dd94485ec172347d8940460e80bcddda0
Wissi00 <118172042+Wissi00@users.noreply.github.com>
2023-01-24 16:09:34 +0100

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,17 @@
+import pygame, sys
+from Buttons import Button
+from Variables import *
+MenuButton=Button(0,0,placeHolder,50,50)
+
+
+def game():
+    from Menu import menu
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                pygame.quit()
+                sys.exit()
+            screen.fill((255,255,255))
+            if MenuButton.drawClick():
+                menu()
+            pygame.display.update()
