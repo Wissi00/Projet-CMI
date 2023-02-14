79d2ca975176f9ec9a0033663082bb910256cb94
Traquenard-Galeo <123472521+Traquenard-Galeo@users.noreply.github.com>
2023-02-14 16:08:30 +0100

    Classe Mur, déplacements horizontaux et collisions

--- classes.py
+++ classes.py
@@ -1,7 +1,5 @@
-<<<<<<< Updated upstream
+
 from Variables import *
-=======
->>>>>>> Stashed changes
 
 class oiseau():
     def __init__(self, imagepiaf, x, y):
@@ -10,20 +8,37 @@ class oiseau():
         self.y = y
         self.dir = 1
         self.vitesseHorizontale = 5
-    
-    def __posistionX__(self):
-        self.x = x + 33
+        self.gravite = 5
+        self.prochainMur = mur1
+
+
+    def posistionX(self):
+        if self.dir == -1:
+            if self.prochainMur.x < (self.x + self.vitesseHorizontale*self.dir) :
+                self.x = self.x + self.vitesseHorizontale*self.dir 
+            else:
+                 self.x + self.x + (self.vitesseHorizontale) - (self.x - self.prochainMur.x)
+                 self.dir = 1
+                 self.prochainMur = mur2
+        else:
+            if self.prochainMur.x > (self.x + self.vitesseHorizontale*self.dir) :
+                self.x = self.x + self.vitesseHorizontale*self.dir 
+            else:
+                 self.x + self.x - (self.vitesseHorizontale) - (self.x - self.prochainMur.x)
+                 self.dir = -1
+                 self.prochainMur = mur1
+
+        return 
+
+    def draw(self):
+        screen.blit(self.sprite,(self.posistionX(),self.y))
         
 
 
 
 class mur():
-    def __init__(self, y):
-        posY = y
-
-    def draw(self):
-        screen.blit(self.sprite,(self.posX,self.posY))
-
+    def __init__(self, x):
+        self.x = x
 
 
 
