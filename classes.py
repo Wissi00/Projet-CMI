fb98c5f96b6d1185cb1ceb2c26f579e5f2b37e37
Traquenard-Galeo <123472521+Traquenard-Galeo@users.noreply.github.com>
2023-02-14 14:36:48 +0100

    ajout classe oiseau
    un oiseau contient sprite, x,y

---
+++ classes.py
@@ -0,0 +1,39 @@
+class oiseau():
+    def __init__(self,imagepiaf,x,y):
+        self.sprite = imagepiaf
+        self.posX = x 
+        self.posY = y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+caca="lénazis"
+
