a2e823dce108e0d446d0a30489765a7db5b85fb5
Wissi00 <118172042+Wissi00@users.noreply.github.com>
2023-02-14 14:58:38 +0100

    creation de la fonction draw de l oiseau et appeler la fonction dans le game() ajout de clock.tick(60) dans le game() pour faire en sorte que le jeu tourne a 60 fps

--- Game.py
+++ Game.py
@@ -1,9 +1,11 @@
 import pygame, sys
 from Variables import *
+from classes import *
+
 MenuButton=Button(0,0,placeHolder,50,50)
 
-
 def game():
+    Player1=oiseau(oiseauImg,250,250)
     from Menu import menu
     while True:
         for event in pygame.event.get():
@@ -11,6 +13,7 @@ def game():
                 pygame.quit()
                 sys.exit()
             screen.fill((255,255,255))
+            Player1.draw()
             if MenuButton.drawClick():
                 menu()
             pygame.display.update()
--- classes.py
+++ classes.py
@@ -1,8 +1,13 @@
+from Variables import *
+
 class oiseau():
     def __init__(self,imagepiaf,x,y):
         self.sprite = imagepiaf
         self.posX = x 
         self.posY = y
+
+    def draw(self):
+        screen.blit(self.sprite,(self.posX,self.posY))
 
 
 
