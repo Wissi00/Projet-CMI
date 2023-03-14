57f20c5cfde7fb4b6a1e84f2c09cb16c2c6fd101
Colindvrt <116315435+Colindvrt@users.noreply.github.com>
2023-03-14 16:14:20 +0100

    Ajout du saut
    Correction du saut pour éviter de monter à l'infini

--- Game.py
+++ Game.py
@@ -5,8 +5,8 @@ from Lost import *
 
 
 MenuButton=Button(0,0,placeHolder,50,50)
-
 def game():
+    jumpdispo = True
     mur1=mur(placeHolder,0,20)
     mur2=mur(placeHolder,480,20)
     Player1=oiseau(oiseauImg,250,250,mur1,mur2)
@@ -24,11 +24,21 @@ def game():
         mur2.draw()
         Player1.mouvX()
         Player1.draw()
-        isJumping = pygame.key.get_pressed()[pygame.K_SPACE]
+        if pygame.key.get_pressed()[pygame.K_SPACE]:
+            if jumpdispo:
+                isJumping = True
+                jumpdispo = False
+            else:
+                isJumping = False
+        else:
+            isJumping = False
+            if jumpdispo == False:
+                jumpdispo = True
+                isJumping = False
         Player1.mouvY(isJumping)
         if Player1.y>500:
             MenuLost()
         if MenuButton.drawClick():
             menu() 
         pygame.display.update()
-        clock.tick(60)
+        clock.tick(100)
