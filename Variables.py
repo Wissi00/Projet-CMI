6cdd3ab6de197e8c9799182f907d0ebc176b40ce
Wissi00 <118172042+Wissi00@users.noreply.github.com>
2023-04-17 10:47:13 +0200

    Trie du ficheir variables.py

--- Variables.py
+++ Variables.py
@@ -5,28 +5,41 @@ from pygame.locals import *
 pygame.init()
 
 taille = [500,500]
+
+#Images---------------------------------------------------------------------------------------------------
+
+#Background
+
 fond = Image.open("sprites/FondA.png")
 fond.save("sprites/Fondr.png")
-fond2 = Image.open("sprites/Fond2.png")
+fond2 = Image.open("sprites/FondA2.png")
 fond2 = fond2.resize((taille[0], taille[1]))
 fond2.save("sprites/Fond2r.png")
+Fond=pygame.image.load("sprites/Fondr.png")
+Fond2=pygame.image.load("sprites/Fond2r.png")
+#Sprites
+
+placeHolder=pygame.image.load("sprites/placeHolder.png")
+piqueImg=pygame.image.load("sprites/pique.png")
+oiseauImg=pygame.image.load("sprites/oiseau.png")
+oiseauImg = pygame.transform.scale(oiseauImg, (80, 80))
+icon = pygame.image.load("sprites/oiseau.png")
+
+#Boutons
+Jouer=pygame.image.load("sprites/Jouer.png")
+Power=pygame.image.load("sprites/Power.png")
+StartButton=Button(taille[0]/2.5,taille[1]/2.22,Jouer,taille[0]/5,taille[1]/10)
+ExitButton=Button(taille[0]/1.15,taille[1]/50,Power,taille[0]/10,taille[1]/10)
+
+#Autres-----------------------------------------------------------------------------------------------
 
 hauteur = 0
 plafond = taille[0]*2/5
 screen = pygame.display.set_mode(taille)
-pixelfont=pygame.font.Font('Pixel.ttf',40)
-scoreTexte=pixelfont.render((str(hauteur)), True, (255,255,255))
-placeHolder=pygame.image.load("sprites/placeHolder.png")
-Jouer=pygame.image.load("sprites/Jouer.png")
-Power=pygame.image.load("sprites/Power.png")
-Fond=pygame.image.load("sprites/Fondr.png")
-Fond2=pygame.image.load("sprites/Fond2r.png")
-piqueImg=pygame.image.load("sprites/pique.png")
-StartButton=Button(taille[0]/2.5,taille[1]/2.22,Jouer,taille[0]/5,taille[1]/10)
-ExitButton=Button(taille[0]/1.15,taille[1]/50,Power,taille[0]/10,taille[1]/10)
-img=pygame.image.load("sprites/oiseau.png")
-oiseauImg = pygame.transform.scale(img, (80, 80))
 clock=pygame.time.Clock()
 rect = screen.get_rect()
-icon = pygame.image.load("sprites/oiseau.png")
-scoreTexte= pixelfont.render(str(hauteur),True,(255,255,255))
+
+#Text-----------------------------------------------------------------------------------------------
+
+pixelfont=pygame.font.Font('Pixel.ttf',40)
+scoreTexte=pixelfont.render((str(hauteur)), True, (255,255,255))
